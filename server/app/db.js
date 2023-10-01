a6403b8d5aa678e740bb273702c0b13f6ec71721
Vlad Sabev <vdsabev@gmail.com>
2023-10-01 13:12:29 +0300

    Fix db connection

--- server/app/db.js
+++ server/app/db.js
@@ -4,23 +4,23 @@ const mongoose = require('mongoose');
 let connection = null;
 
 const connect = async function () {
-  if (!connection) {
-    // Assign first, await later to avoid multiple function calls creating new connections
-    connection = mongoose.connect(process.env.MONGO_DB_URL, {
-      serverSelectionTimeoutMS: 5000,
-    });
+  if (connection) return connection;
 
-    await connection;
+  // Assign first, await later to avoid multiple function calls creating new connections
+  connection = mongoose.connect(process.env.MONGO_DB_URL, {
+    serverSelectionTimeoutMS: 5000,
+  });
 
-    mongoose.model(
-      'data',
-      new mongoose.Schema({
-        lanes: [{ name: String, tasks: [String] }],
-      })
-    );
+  await connection;
 
-    return connection;
-  }
+  mongoose.model(
+    'data',
+    new mongoose.Schema({
+      lanes: [{ name: String, tasks: [String] }],
+    })
+  );
+
+  return connection;
 };
 
 module.exports = {

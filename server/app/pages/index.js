d835c1ba83b44b243a2764f1d833626b0f527de8
Vlad Sabev <vdsabev@gmail.com>
2023-10-02 10:35:10 +0300

    Try path.join

--- server/app/pages/index.js
+++ server/app/pages/index.js
@@ -1,5 +1,7 @@
 const ejs = require('ejs');
+const path = require('path');
 
-exports.App = (data) => ejs.renderFile(`${__dirname}/app.html`, data);
+exports.App = (data) => ejs.renderFile(path.join(__dirname, 'app.html'), data);
 
-exports.Task = (data) => ejs.renderFile(`${__dirname}/task.html`, data);
+exports.Task = (data) =>
+  ejs.renderFile(path.join(__dirname, 'task.html'), data);

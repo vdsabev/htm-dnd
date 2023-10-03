996d9ed5290db26144fe502ddea36bee4053a5a2
Vlad Sabev <vdsabev@gmail.com>
2023-10-03 22:27:34 +0300

    More tests

--- public/src/actions.test.js
+++ public/src/actions.test.js
@@ -1,20 +1,65 @@
 import assert from 'assert';
 import actions from './actions.js';
 
+const data = {
+  empty: { lanes: [] },
+  default: {
+    lanes: [
+      { name: 'A', tasks: [] },
+      { name: 'B', tasks: [] },
+      { name: 'C', tasks: [] },
+    ],
+  },
+};
+
 const suite = {
   addLane: {
     'should return new lane'() {
       assert.deepStrictEqual(
-        actions.addLane.call({ lanes: [] }),
-        //
+        actions.addLane.call(data.empty), //
         { lanes: [{ name: 'New Lane', tasks: [] }] }
+      );
+    },
+  },
+
+  removeLane: {
+    'should remove existing lane'() {
+      assert.deepStrictEqual(
+        actions.removeLane.call(data.default, data.default.lanes[0]),
+        { ...data.default, lanes: data.default.lanes.slice(1) }
+      );
+    },
+
+    'should not remove non-existing lane'() {
+      assert.deepStrictEqual(
+        actions.removeLane.call(data.default, {}),
+        data.default
+      );
+    },
+  },
+
+  updateLane: {
+    'should update existing lane name'() {
+      assert.deepStrictEqual(
+        actions.updateLane.call(data.default, data.default.lanes[0], 'D'),
+        {
+          ...data.default,
+          lanes: [{ name: 'D', tasks: [] }, ...data.default.lanes.slice(1)],
+        }
+      );
+    },
+
+    'should not update non-existing lane'() {
+      assert.deepStrictEqual(
+        actions.updateLane.call(data.default, { name: 'D', tasks: [] }, 'D'),
+        data.default
       );
     },
   },
 };
 
 for (const [description, tests] of Object.entries(suite)) {
-  console.log(description);
+  console.log(`\n${description}`);
   for (const [message, test] of Object.entries(tests)) {
     try {
       test();

40f21834a4d375f3f4e9b90364f190427bf76f90
Vlad Sabev <vdsabev@gmail.com>
2023-10-04 23:54:03 +0300

    Switch to 'concurrently'

--- public/src/components/Board.js
+++ public/src/components/Board.js
@@ -10,7 +10,7 @@ export default ({ board, actions }) => html`
   <div class="min-w-[var(--lane-width)]">
     <button
       type="button"
-      class="self-start p-10 bg-white border rounded text-left hover:pr-[calc(var(--lane-width)-2.5rem)] transition-[padding]"
+      class="self-start p-3 bg-white border rounded text-left hover:pr-[calc(var(--lane-width)-2.5rem)] transition-[padding]"
       onclick=${(event) => {
         actions.addLane();
         event.currentTarget.scrollIntoView({ behavior: 'smooth' });
